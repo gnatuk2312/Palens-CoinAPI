c165024710c2dfd3dfc2d5d7186e94af4e3592e1
gnatuk2312 <gnat.liashenko@gmail.com>
2022-04-25 16:00:48 +0300

    responsive layout, styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import React from 'react';
 import './App.css';
 
 function App() {
 	return (
-		<div className="App">
-			Helo World
-		</div>
+		<section className="app">
+			<article className='app-wrapper'>
+				<form className='app-form'>
+					<input type='text' className='input' placeholder='Назва криптовалюти' />
+					<button type='submit' className='button'>Subscribe</button>
+				</form>
+				<div className='app-data-wrapper'>
+					<p className='app-data-title'>Market data:</p>
+					<ul className='app-data'>
+						<li className='app-data-item'>
+							<span>Symbol:</span>
+							<p>BTC</p>
+						</li>
+						<li className='app-data-item'>
+							<span>Price:</span>
+							<p>$10000</p>
+						</li>
+						<li className='app-data-item'>
+							<span>Time:</span>
+							<p>Aug 7, 9:45 AM</p>
+						</li>
+					</ul>
+				</div>
+				<div className='app-chart-wrapper'>
+					<p className='app-chart-title'>Charting data:</p>
+					<div className='app-chart'>
+						<div></div>
+					</div>
+				</div>
+			</article>
+		</section>
 	);
 }
 
